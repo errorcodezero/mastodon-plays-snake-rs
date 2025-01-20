a546f343ef5eb75cdeba753b657b30d266c5f166
ErrorCode0 <me@errorcodezero.dev>
2025-01-20 12:46:46 -0800

    Move function and more stuff(not working yet)

--- src/game.rs
+++ src/game.rs
@@ -1,6 +1,8 @@
+use core::panic;
+
 use rand::{Rng, thread_rng};
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, PartialEq, Eq)]
 pub enum Block {
     SnakeBody,
     SnakeHead,
@@ -29,6 +31,8 @@ impl Direction {
 
 pub struct Game {
     grid: [[Block; 5]; 5],
+    // Snake vector is reversed where the head is the last element and the rest of the elements
+    // comprise of the body.
     snake: Vec<(usize, usize)>,
     food: (usize, usize),
     score: u8,
@@ -63,31 +67,89 @@ impl Game {
             coord.1 = rng.gen_range(0..5);
         }
 
-        self.food = coord;
-
         self.update_grid();
     }
 
     fn update_grid(&mut self) {
         self.grid = [[Block::Empty; 5]; 5];
-        self.grid[self.snake[0].1][self.snake[0].0] = Block::SnakeHead;
+        self.grid[self.snake[self.snake.len() - 1].1][self.snake[self.snake.len() - 1].0] =
+            Block::SnakeHead;
         self.snake
             .iter()
+            .rev()
             .skip(1)
             .for_each(|x| self.grid[x.1][x.0] = Block::SnakeBody);
         self.grid[self.food.1][self.food.0] = Block::Food;
     }
 
+    fn move_food(&mut self) {
+        let mut rng = thread_rng();
+        self.food.0 = rng.gen_range(0..5);
+        self.food.1 = rng.gen_range(0..5);
+        while self.grid[self.food.1][self.food.0] == Block::SnakeBody
+            || self.grid[self.food.1][self.food.0] == Block::SnakeHead
+        {
+            self.food.0 = rng.gen_range(0..5);
+            self.food.1 = rng.gen_range(0..5);
+        }
+
+        self.update_grid();
+    }
+
     pub fn move_snake(&mut self, direction: Direction) {
         if let Some(i) = &self.direction {
             let valid_moves = i.get_valid_dir();
-            if direction == valid_moves.0 || direction == valid_moves.1 || direction == valid_moves.2 {
+            let mut change_x = 0;
+            let mut change_y = 0;
+            if direction == valid_moves.0
+                || direction == valid_moves.1
+                || direction == valid_moves.2
+            {
+                // Change in y is reversed since a smaller y value means an array closer to the
+                // top. Change in x is the still the same though.
                 match direction {
-                    Direction::Up => {},
-                    Direction::Down => {}
-                    Direction::Left => {}
-                    Direction::Right => {}
+                    Direction::Up => change_y -= 1,
+                    Direction::Down => change_y += 1,
+                    Direction::Left => change_x -= 1,
+                    Direction::Right => change_x += 1,
                 }
+            } else {
+                panic!("Direction is not valid.");
+            }
+            let mut coord = self.snake[self.snake.len() - 1];
+            coord.1 += change_y;
+            coord.0 += change_x;
+            self.snake.push(coord);
+
+            if coord.0 != self.food.0 || coord.1 != self.food.1 {
+                self.snake.remove(0);
+                self.update_grid();
+            } else {
+                self.inc_score();
+                self.move_food();
+            }
+        } else {
+            let mut change_x = 0;
+            let mut change_y = 0;
+
+            match direction {
+                Direction::Up => change_y -= 1,
+                Direction::Down => change_y += 1,
+                Direction::Left => change_x -= 1,
+                Direction::Right => change_x += 1,
+            }
+
+            let mut coord = self.snake[self.snake.len() - 1];
+            coord.1 += change_y;
+            coord.0 += change_x;
+            self.snake.push(coord);
+
+            if coord.0 != self.food.0 || coord.1 != self.food.1 {
+                self.snake.remove(0);
+                self.update_grid();
+            } else {
+                self.inc_score();
+                self.move_food();
             }
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,8 @@ pub mod game;
 fn main() {
     let mut game = Game::new();
     game.setup();
-    let mut dir = Direction::Down;
 
-    dir = dir.get_valid_dir().0;
-
-    println!("{:?}", dir);
-
-    println!("{}", &game.to_string());
-    game.inc_score();
-    println!("Score: {}", &game.get_score());
+    println!("{}", game.to_string());
+    game.move_snake(Direction::Up);
+    println!("{}", game.to_string());
 }
